fff3a8368a76e3f947395b45bfa7557a117fd748
ocorvu <professorjoaodamata@gmail.com>
2022-12-28 21:58:38 -0300

    dropdown basico em navbar para categorias e produtos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,28 +21,49 @@
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-            </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="{{ route('products.show') }}" class="
-                @if (request()->route()->uri === 'products')
-                  active
-                @else
-                  not-active
-                @endif" aria-current="page">Products</a>
-  
-                <a href="{{ route('products.create') }}" class="
-                  @if (request()->route()->uri === 'create')
-                   active
-                  @else
-                    not-active
-                  @endif 
-                  ">New</a>
+          <div class="flex-shrink-0">
+            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+          </div>
+          {{-- Products --}}
+          <div x-data="{open: false}" class="relative">
+            <button 
+              class="ml-10 
+              @if (request()->route()->uri === 'products')
+                active
+              @else
+                not-active
+              @endif"
+              x-on:click="open = ! open">
+              Products
+            </button>
+            <div class="absolute left-0 ml-10 active" x-show="open">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <div class="flex flex-col">
+                <a href="{{ route('products.show') }}" class="not-active">All</a>
+                <a href="{{ route('products.create') }}" class="not-active">New</a>
               </div>
             </div>
+          </div>
+          {{-- Categories --}}
+          <div x-data="{open: false}" class="relative">
+            <button 
+              class="ml-10 
+              @if (request()->route()->uri === 'categories')
+                active
+              @else
+                not-active
+              @endif"
+              x-on:click="open = ! open">
+              Categories
+            </button>
+            <div class="absolute left-0 ml-10 active" x-show="open">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <div class="flex flex-col">
+                <a href="{{ route('categories.show') }}" class="not-active">All</a>
+                <a href="{{ route('categories.create') }}" class="not-active">New</a>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </div>
