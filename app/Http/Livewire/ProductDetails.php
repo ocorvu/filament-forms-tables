a585aaf863d06b6f3939d1a2436203c17ad5dfc4
ocorvu <professorjoaodamata@gmail.com>
2022-12-28 21:55:52 -0300

    validação de thumbnail

--- app/Http/Livewire/ProductDetails.php
+++ app/Http/Livewire/ProductDetails.php
@@ -2,21 +2,30 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
 
 class ProductDetails extends Component
 {
     public Product $product;
-
+    
     public function render()
     {
+        if (str_starts_with($this->product->thumbnail,'thumbnail') ) {
+
+            $thumbnail = "storage/{$this->product->thumbnail}";
+
+        } else if ($this->product->thumbnail === NULL) {
+            $thumbnail = false;
+        } else {
+            $thumbnail = $this->product->thumbnail;
+        }
+
         return view('livewire.product-details', [
             'name' => $this->product->name,
-            'category' => $this->product->category()->first(),
+            'category' => $this->product->category,
             'description' => $this->product->description,
-            'thumbnail' => $this->product->thumbnail,
+            'thumbnail' => $thumbnail,
             'barcode' => $this->product->barcode,
             'quantity' => $this->product->quantity,
             'price' => $this->product->price,
