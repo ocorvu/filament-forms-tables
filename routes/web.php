0e8f482d77d5bcbbac64dd169de4cad5ad8f41ba
ocorvu <professorjoaodamata@gmail.com>
2022-12-28 21:46:43 -0300

    rota para detalhes de categoria

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Livewire\CategoryDetails;
 use App\Http\Livewire\CreateCategory;
 use App\Http\Livewire\CreateProduct;
 use App\Http\Livewire\EditProduct;
 use App\Http\Livewire\ProductDetails;
 use App\Http\Livewire\ShowCategories;
 use App\Http\Livewire\ShowProducts;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/edit/{product}', EditProduct::class)->name('products.edit');
+Route::get('/products/edit/{product}', EditProduct::class)->name('products.edit');
 
 Route::get('/products/create', CreateProduct::class)->name('products.create');
 
@@ -23,6 +23,6 @@ Route::get('/products/{product}', ProductDetails::class)->name('products.details
 
 Route::get('/categories', ShowCategories::class)->name('categories.show');
 
-
 Route::get('/categories/create', CreateCategory::class)->name('categories.create');
 
+Route::get('/categories/{category}', CategoryDetails::class)->name('categories.details');
